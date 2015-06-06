85e2ad41706b5af806439f73580070f9078e5ebb
Rouven Dreimann <rdreimann@fh-bielefeld.de>
2015-06-07 00:02:31 +0200

    public umbenannt

--- app.js
+++ app.js
@@ -18,15 +18,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// uncomment after placing your favicon in /_public
+//app.use(favicon(__dirname + '/_public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public/app')));
-//app.use(express.static(path.join(__dirname, 'public/')));
-app.use(express.static(path.join(__dirname, 'public/app/')));
+app.use(require('less-middleware')(path.join(__dirname, '_public/app')));
+//app.use(express.static(path.join(__dirname, '_public/')));
+app.use(express.static(path.join(__dirname, '_public/app/')));
 
 //app.use(multer());
 
